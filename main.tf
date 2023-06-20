4d961ac306a8a5462ad565fb34d89feb59326fe1
Jimmy P <bunches_stands_0s@icloud.com>
2023-06-20 09:19:19 -0500

    set up terraform cloud

---
+++ main.tf
@@ -0,0 +1,19 @@
+# The configuration for the `remote` backend.
+terraform {
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "example-org-0ea620"
+
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "getting-started"
+    }
+  }
+}
+
+# An example resource that does nothing.
+resource "null_resource" "example" {
+  triggers = {
+    value = "A example resource that does nothing!"
+  }
+}
