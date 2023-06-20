97bd1b273eb88d2d0d75e40c89a3f091ab4b2ea2
Jimmy P <bunches_stands_0s@icloud.com>
2023-06-20 16:35:46 -0500

    tf format

--- main.tf
+++ main.tf
@@ -7,11 +7,11 @@ terraform {
 }
 
 resource "digitalocean_droplet" "www-1" {
-    image = "ubuntu-20-04-x64"
-    name = "www-1"
-    region = "nyc3"
-    size = "s-1vcpu-1gb"
-    ssh_keys = [
-      data.digitalocean_ssh_key.terraform.id
-    ]
+  image  = "ubuntu-20-04-x64"
+  name   = "www-1"
+  region = "nyc3"
+  size   = "s-1vcpu-1gb"
+  ssh_keys = [
+    data.digitalocean_ssh_key.terraform.id
+  ]
 }
