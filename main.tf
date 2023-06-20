83ffd8772e6181270d2f3f71743db15c9e5446e7
Jimmy P <bunches_stands_0s@icloud.com>
2023-06-20 15:53:56 -0500

    setting up self-hosted runner

--- main.tf
+++ main.tf
@@ -6,32 +6,12 @@ terraform {
   }
 }
 
-# An example resource that does nothing.
-resource "null_resource" "example" {
-  triggers = {
-    value = "A example resource that does nothing!"
-  }
-}
-
-resource "fakewebservices_vpc" "primary_vpc" {
-  name       = "Primary VPC"
-  cidr_block = "0.0.0.0/1"
-}
-
-resource "fakewebservices_server" "servers" {
-  count = 2
-
-  name = "Server ${count.index + 1}"
-  type = "t2.micro"
-  vpc  = fakewebservices_vpc.primary_vpc.name
-}
-
-resource "fakewebservices_load_balancer" "primary_lb" {
-  name    = "Primary Load Balancer"
-  servers = fakewebservices_server.servers[*].name
-}
-
-resource "fakewebservices_database" "prod_db" {
-  name = "Production DB"
-  size = 256
+resource "digitalocean_droplet" "www-1" {
+    image = "ubuntu-20-04-x64"
+    name = "www-1"
+    region = "nyc3"
+    size = "s-1vcpu-1gb"
+    ssh_keys = [
+      data.digitalocean_ssh_key.terraform.id
+    ]
 }
