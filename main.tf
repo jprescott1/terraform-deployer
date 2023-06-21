2d7d43ae22afc3695d47b4d18dfd74867818c81f
Jimmy P <bunches_stands_0s@icloud.com>
2023-06-21 18:25:30 -0500

    remove droplet

--- main.tf
+++ main.tf
@@ -5,12 +5,12 @@ terraform {
   }
 }
 
-resource "digitalocean_droplet" "www-1" {
-  image  = "ubuntu-20-04-x64"
-  name   = "www-1"
-  region = "nyc3"
-  size   = "s-1vcpu-1gb"
-  # ssh_keys = [
-  #   data.digitalocean_ssh_key.terraform.id
-  # ]
-}
+# resource "digitalocean_droplet" "www-1" {
+#   image  = "ubuntu-20-04-x64"
+#   name   = "www-1"
+#   region = "nyc3"
+#   size   = "s-1vcpu-1gb"
+#   ssh_keys = [
+#     data.digitalocean_ssh_key.terraform.id
+#   ]
+# }
