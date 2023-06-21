3a1f5cc87649e3e7ae59dea2587a3e602910f9c9
Jimmy P <bunches_stands_0s@icloud.com>
2023-06-21 15:59:44 -0500

    try creating droplet without ssh key for now

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "digitalocean_droplet" "www-1" {
   name   = "www-1"
   region = "nyc3"
   size   = "s-1vcpu-1gb"
-  ssh_keys = [
-    data.digitalocean_ssh_key.terraform.id
-  ]
+  # ssh_keys = [
+  #   data.digitalocean_ssh_key.terraform.id
+  # ]
 }
